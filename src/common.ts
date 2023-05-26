9e86bdf418c453ba3357b72529ceaeb019bcc97e
madkne-laptop <madkne@laptop>
2023-05-26 09:53:13 +0430

    complete file uploader widget in material - version 0.146

--- src/common.ts
+++ src/common.ts
@@ -6,7 +6,7 @@ import { error } from '@dat/lib/log';
 import { UIFrameWorkType } from './types';
 
 
-export const VERSION = '0.145';
+export const VERSION = '0.146';
 
 export async function detectAngularSourcePath() {
     let cwdir = await cwd();
